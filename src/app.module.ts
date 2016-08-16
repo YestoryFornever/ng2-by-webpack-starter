b6d07ccfe499a69a17e56e270719acd7a56447f7
YestoryFornever <guosj90@163.com>
2016-08-16 17:18:54 +0800

    引入es6-shim、polyfills、zone.js、reflect-metadata
    首先引入 es6-shim ，它会给全局对象 window 打上补丁，使其支持 ES2015(ES6) 的必备特性。 然后是 Angular 2
    的 polyfills ，以及 zone.js 和 reflect-metadata 。
    添加一些注释

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';//浏览器运行所需要的一切
 import { AppComponent }  from './app/app.component';
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule ],//[其它]模块，用来导出当前模块所需的“素材”。几乎每个应用的 根模块 都要导入 BrowserModule 模块
+  declarations: [ AppComponent ],//从属于当前模块的组件和指令
+  bootstrap:    [ AppComponent ]//标记出[根组件]，在启动应用时，Angular 应该通过它来进行引导
 })
 export class AppModule { }
