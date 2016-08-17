c358ebd10c880429e9d8c4f17f0c36c651eae2b9
YestoryFornever <guosj90@163.com>
2016-08-17 13:59:44 +0800

    添加dashboard组件及路由

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,6 @@
 /*
- *	数据使用者并不知道本服务会如何获取数据。 我们的 HeroService 服务可以从任何地方获取英雄的数据。 它可以从网络服务器获取，可以从浏览器的局部存储区获取，也可以是直接写在源码中的 mock 数据。
- *	这就是从组件中移除数据访问代码的美妙之处。 这样我们可以随时改变数据访问的实现方式，而无需对使用英雄的组件作任何改动。
+ *  数据使用者并不知道本服务会如何获取数据。 我们的 HeroService 服务可以从任何地方获取英雄的数据。 它可以从网络服务器获取，可以从浏览器的局部存储区获取，也可以是直接写在源码中的 mock 数据。
+ *  这就是从组件中移除数据访问代码的美妙之处。 这样我们可以随时改变数据访问的实现方式，而无需对使用英雄的组件作任何改动。
  */
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
@@ -8,12 +8,16 @@ import {Hero} from './hero';
 
 @Injectable()
 export class HeroService {
-	getHeroes(){
-		return Promise.resolve(HEROES);
-	}
-	getHeroesSlowly() {
-		return new Promise<Hero[]>(resolve =>
-		setTimeout(() => resolve(HEROES), 2000) // 2 seconds
-		);
-	}
+  getHeroes(){
+    return Promise.resolve(HEROES);
+  }
+  getHeroesSlowly() {
+    return new Promise<Hero[]>(resolve =>
+    setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+    );
+  }
+  getHero(id: number) {
+    return this.getHeroes()
+               .then(heroes => heroes.find(hero => hero.id === id));
+  }
 }
--- src/hero-detail/hero-detail.component.ts
+++ src/hero-detail/hero-detail.component.ts
@@ -1,12 +1,28 @@
-import {Component,Input} from '@angular/core';
+import { Component,Input,OnInit } from '@angular/core';
 
-import {Hero} from '../app/hero'
+import { Hero } from '../app/hero'
+import { ActivatedRoute, Params } from '@angular/router';
+import { HeroService } from '../app/hero.service';
 
 @Component({
-	selector:'my-hero',
-	templateUrl:'src/hero-detail/hero-detail.component.html',
-	styleUrls: ['src/hero-detail/hero-detail.component.css'] 
+  selector:'my-hero',
+  templateUrl:'src/hero-detail/hero-detail.component.html',
+  styleUrls: ['src/hero-detail/hero-detail.component.css'] 
 })
-export class HeroDetailComponent{ 
-	@Input()hero:Hero;
+export class HeroDetailComponent implements OnInit{ 
+  @Input()hero:Hero;
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute) {
+  }
+  ngOnInit() {
+    this.route.params.forEach((params: Params) => {
+      let id = +params['id'];
+      this.heroService.getHero(id)
+        .then(hero => this.hero = hero);
+    });
+  }
+  goBack() {
+    window.history.back();
+  }
 }
