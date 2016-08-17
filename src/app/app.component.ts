72dcafc9d061fce431460ba9bd6980130dc7692f
YestoryFornever <guosj90@163.com>
2016-08-17 10:05:49 +0800

    数据获取封装成服务

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,24 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
+import { HeroService } from './hero.service';
 import {Hero} from './hero';
-
-const HEROES: Hero[] = [
-	{ id: 11, name: 'Mr. Nice' },
-	{ id: 12, name: 'Narco' },
-	{ id: 13, name: 'Bombasto' },
-	{ id: 14, name: 'Celeritas' },
-	{ id: 15, name: 'Magneta' },
-	{ id: 16, name: 'RubberMan' },
-	{ id: 17, name: 'Dynama' },
-	{ id: 18, name: 'Dr IQ' },
-	{ id: 19, name: 'Magma' },
-	{ id: 20, name: 'Tornado' }
-];
-
 @Component({
 	selector: 'my-app',
 	templateUrl:'src/app/app.component.html',
-	styleUrls: ['src/app/app.component.css'] 
+	styleUrls: ['src/app/app.component.css'],
+	providers: [HeroService] 
 })
-export class AppComponent { 
-	heroes = HEROES;
+export class AppComponent implements OnInit{ 
 	title = 'Tour of Heroes';
 	selectedHero:Hero;
+	heroes:Hero[];
+	ngOnInit() {
+		this.getHeroes();
+	}
+	constructor(private heroService: HeroService) { }
+	getHeroes() {
+		this.heroes = this.heroService.getHeroes();
+	}
 	onSelect(hero: Hero) { this.selectedHero = hero; }
 }
---
+++ src/app/hero.service.ts
@@ -0,0 +1,13 @@
+/*
+ *	数据使用者并不知道本服务会如何获取数据。 我们的 HeroService 服务可以从任何地方获取英雄的数据。 它可以从网络服务器获取，可以从浏览器的局部存储区获取，也可以是直接写在源码中的 mock 数据。
+ *	这就是从组件中移除数据访问代码的美妙之处。 这样我们可以随时改变数据访问的实现方式，而无需对使用英雄的组件作任何改动。
+ */
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+	getHeroes(){
+		return HEROES;
+	}
+}
